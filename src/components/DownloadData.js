98df28d91091b6ea74655d58b2fb6c25b2eb549f
huajunchen <35971289+chen5656@users.noreply.github.com>
2021-07-22 14:57:37 -0500

    add function to download data as excel
    download done

--- src/components/DownloadData.js
+++ src/components/DownloadData.js
@@ -6,28 +6,35 @@ const DownloadExcel = (props) => {
 
     useEffect(() => {
         if(props.downloadData){
-            const fields = props.downloadData[0].fields.map(f => f.name);
-            let dataArray= props.downloadData.map(layer=>{
-                return ([
-                    fields,
-                    ...layer.features.map(f=>{
-                       return fields.map(field=>f.attributes[field])                         
+            var header =  props.downloadData[0].fields.map(f => f.name)
+                .filter(item=>item!=='LocationDate')
+                .concat(['dateTime', 'latitude', 'longitude']);           
+            let dataArray = props.downloadData.map(layer => {
+                return (
+                    layer.features.map(f=>{
+                        let newF={...f.attributes};
+                        newF.latitude=f.geometry.x
+                        newF.longitude=f.geometry.y
+                        newF.dateTime =new Date(f.attributes['LocationDate']).toLocaleString().replace(',', ' ');
+                        delete newF.LocationDate;
+                        delete newF.ESRI_OID;
+                        return newF;
                     })
-                ]);
+                )
             })
-            downloadxls(dataArray);
+            downloadxls(dataArray,header);
             props.setDownloadData(null);
         }
     }, [props.downloadData]);
 
-    const downloadxls = (dataArray)=>{
+    const downloadxls = (dataArray,header)=>{
         let wb = XLSX.utils.book_new();
         dataArray.forEach((data,i)=>{
-            let ws = XLSX.utils.json_to_sheet(data);
+            let ws = XLSX.utils.json_to_sheet(data, {header: header});
             XLSX.utils.book_append_sheet(wb, ws, `sheet${i}`);
         })
-        let buf = XLSX.write(wb, {bookType:'xlsx', type:'buffer'}); // generate a nodejs buffer
-        let str = XLSX.write(wb, {bookType:'xlsx', type:'binary'}); // generate a binary string in web browser
+        // let buf = XLSX.write(wb, {bookType:'xlsx', type:'buffer'}); // generate a nodejs buffer
+        // let str = XLSX.write(wb, {bookType:'xlsx', type:'binary'}); // generate a binary string in web browser
         XLSX.writeFile(wb, `undefined.xlsx`);
     }
     return null
